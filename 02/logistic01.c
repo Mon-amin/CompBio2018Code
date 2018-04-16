71875ff97e6066964af7b6f1c3701b467c3020d8
Mon-amin <dr.kumamon8996@gmail.com>
2018-04-16 15:04:05 +0900

    5

--- 02/logistic01.c
+++ 02/logistic01.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 int main(void){
- int t,r,k,x,xx;
+ int t;
+ double r,k,x,xx;
+ k=100
  FILE *fp;
  fp=fopen("output.dat","a");
- fprintf(fp, "%d, %d", 0, x0);
 
+for(r=1.0; r<3.0; r=r+0.1;){
  for(t=0; t<300; t++){
-  xx=x+r*(1-(double)x/k)*x;
-  fprintf(fp, "%d, %d", t, xx);
-   x=xx
+  xx=x+r*(1-x/k)*x;
+  fprintf(fp, "%d, %f\n", t, xx);
+   x=xx;
  }
-
+}
 fclose(fp);
 return 0;
 }
