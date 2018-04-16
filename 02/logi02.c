e65878ecd40395df01235367732c8e9a6975380e
Mon-amin <dr.kumamon8996@gmail.com>
2018-04-16 16:15:17 +0900

    mm

--- 02/logi02.c
+++ 02/logi02.c
@@ -4,10 +4,10 @@ int main(void){
  double r,k,x,xx;
  k=100;
  FILE *fp;
- fp=fopen("output-logi02.dat","a");
+ fp=fopen("output-logi.dat","a");
 
 for(r=1.0; r<3.0; r=r+0.1){
-x=0;
+x=5.0
  for(t=0; t<300; t++){
   xx=x+r*(1-x/k)*x;
   fprintf(fp, "%f, %d, %f\n",r, t, xx);
