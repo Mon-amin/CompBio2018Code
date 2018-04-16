2386a218bacc93a6d3c7a9798eb91b98d021f90e
Mon-amin <dr.kumamon8996@gmail.com>
2018-04-16 15:06:22 +0900

    n

--- 02/logistic01.c
+++ 02/logistic01.c
@@ -2,11 +2,11 @@
 int main(void){
  int t;
  double r,k,x,xx;
- k=100
+ k=100;
  FILE *fp;
  fp=fopen("output.dat","a");
 
-for(r=1.0; r<3.0; r=r+0.1;){
+for(r=1.0; r<3.0; r=r+0.1){
  for(t=0; t<300; t++){
   xx=x+r*(1-x/k)*x;
   fprintf(fp, "%d, %f\n", t, xx);
