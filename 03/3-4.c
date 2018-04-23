68a81ddd0e3873fadb758a76a94e15da06a4f0ca
Mon-amin <dr.kumamon8996@gmail.com>
2018-04-23 14:30:56 +0900

    n

---
+++ 03/3-4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+  int i;
+  for (i = 0; i < 50; i++) {
+    if(i%3==0){
+      printf("3の倍数\n");
+    }
+    else if(i%3==1){
+      printf("余り1\n");
+    }
+  }
+  return 0;
+}
