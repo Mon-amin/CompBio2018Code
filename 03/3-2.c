ed5b66ef25b390a9847b29c100d880516f3745c8
Mon-amin <dr.kumamon8996@gmail.com>
2018-04-23 14:11:49 +0900

    v

--- 03/3-2.c
+++ 03/3-2.c
@@ -5,16 +5,16 @@ int main(void) {
  for(i=0; i<10; i++){
    printf("%d\n", i);
    if(i>5){
-     printf("iは5より大きい,");
+     printf("iは5より大きい\n");
    }
    if(i==3){
-     printf("iは3と等しい,");
+     printf("iは3と等しい\n");
    }
    if((i>=3)&&(i<=6)){
-     printf("iは3以上6以下,");
+     printf("iは3以上6以下\n");
    }
    if ((!i==1||i==2)) {
-     printf("iは1または2ではない,");
+     printf("iは1または2ではない\n");
    }
    printf("\n");
  }
