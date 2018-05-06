b09bcdbad30ba774ed94ca44aa8fa56528f9d0c0
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-07 02:20:03 +0900

    k

--- 03/3-7ex.c
+++ 03/3-7ex.c
@@ -9,14 +9,14 @@ int main(void){
   x=x0;
   xa=x0;
   r=1.3;
-  dt=0.001;
+  dt=0.1;
   k=100;
 
   FILE*fp;
-  fp=fopen("3-7exa.dat","w");
+  fp=fopen("3-7exb.dat","w");
   fprintf(fp,"%f, %f, %f\n", 0.0, xa, x);
 
-  for(i=1;i<10000;i++){
+  for(i=1;i<100;i++){
   xa=k/(1+(k/x0-1)*exp(-r*dt*i));
 
   xx=x+dt*r*(1-x/k)*x;
