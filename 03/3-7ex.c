6999fb8b2470dd5d8c61df2b947ee41b0a5c1e08
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-07 02:00:17 +0900

    d

---
+++ 03/3-7ex.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void){
+  int i,k;
+  double x0,xx,x,r,xa;
+  double dt;
+  x0=10.0;
+  x=x0;
+  xa=x0;
+  r=1.3;
+  dt=0.1;
+  k=100;
+
+  FILE*fp;
+  fp=fopen("3-7ex.dat","w");
+  fprintf(fp,"%f, %f, %f\n", 0.0, xa, x);
+
+  for(i=1;i<1000;i++){
+  xa=k/(1+(k/x0-1)*exp(-r*dt*i));
+
+  xx=x+dt*r*(1-x/k)*x;
+  x=xx;
+
+  fprintf(fp, "%f, %f, %f\n", dt*i, xa, x);
+  }
+  fclose(fp);
+  return 0;
+}
