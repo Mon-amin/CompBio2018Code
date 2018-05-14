4055555521d7f3c71f1de5701cb618a655362c4a
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-14 15:22:13 +0900

    k

---
+++ 05/5-c.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+  int r,t,i;
+  int x[5];
+  srand(time(NULL));
+  for(t=0;t<5;t++){
+    x[t]=0;
+  }
+  FILE *fp;
+
+  fp=fopen("5-c.dat","w");
+
+  for(i=0;i<100;i++){
+    for(t=0;t<5;t++){
+      r=rand()%2;
+      if(r==0){
+        x[t]=x[t]+1;
+      }
+      else{
+        x[t]=x[t]-1;
+      }
+      fprintf(fp,"%d",x[t]);
+    }
+    fprintf(fp,"\n");
+    }
+  }
+  fclose(fp);
+  return 0;
+}
