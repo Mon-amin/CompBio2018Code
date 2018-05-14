468dbebbc94967db750ffe94186f4391a56aa6de
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-14 15:01:49 +0900

    ee

--- 05/5-a.c
+++ 05/5-a.c
@@ -1,23 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(void){
-  int r,i,M,N,x,k;
+  int r,i,M,N,x,k,t;
+  int n=1000 //回数
+  double ave;
+  srand(tome(NULL));
   M=1;
   N=6;
   x=0;
+  t=0;
 
- for(k=0;k<5;k++){
-   srand(k);
+printf("n=%d\n", n);
+
+ for(k=0;k<n;k++){
   for(i=0;i<100;i++){
     r=rand()%(N-M+1)+M;
     x=x+r;
     if(x>=100){
-     printf("%d ,%d\n",k,i);
+     printf("%d",i);
+     t+=i;
+     x=0;
      break;
     }
-
   }
  }
+ printf("\n");
+ ave=(double)t/n;
+ printf("%f\n", ave);
+ 
  return 0;
 }
