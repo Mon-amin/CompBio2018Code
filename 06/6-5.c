3e4ff0d9b06fdb906034b4806313309fee24749c
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-21 14:30:17 +0900

    11

--- 06/6-5.c
+++ 06/6-5.c
@@ -6,14 +6,16 @@ int	main(void){
 		double	x1,x2,y1,y2,i,N;
 		srand(time(NULL));
 		N=1;
-		FILE	*fp;
+
+		FILE *fp;
 		fp=fopen("output_rand.csv","w");
+
 		for(i=0;i<10000;i++){
 				x1=(((double)rand()+1)	/	((double)RAND_MAX	+	1))	*	N;
 				x2=(((double)rand()+1)	/	((double)RAND_MAX	+	1))	*	N;
 				y1=sqrt(-2*log(x1))*cos(2*M_PI*x2);
 				y2=sqrt(-2*log(x1))*sin(2*M_PI*x2);
-				fprin\(fp,"%f,	%f\n%f,	%f\n",x1,y1,x2,y2);
+				fprintf(fp,"%f,	%f\n%f,	%f\n",x1,y1,x2,y2);
 		}
 		fclose(fp);
 		return	0;
