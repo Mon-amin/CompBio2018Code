5c6d9018cd716d8d9e7fd7e110824cb29725454a
Mon-amin <dr.kumamon8996@gmail.com>
2018-05-21 14:13:05 +0900

    6

--- 06/6-2.c
+++ 06/6-2.c
@@ -1,17 +1,21 @@
 #include	<stdio.h>
+
 int	main(void){
+
 		FILE	*fp;
 		int	ret;
+
 		int	i,j;
 		char	name1[100],name2[100];
 		int	no[100],notemp;
 		double	value[100],valuetemp;
+
 		//データを読み込む                                
 			fp=fopen("6-1.csv","r");
-		fscanf(fp,"%[^,],	%s",	name1,name2);
+		fscanf(fp,"%[^_],	%s",	name1,name2);
+
 		i=0;
-		while((ret=fscanf(fp,"%d,	%lf",&notemp,
-&valuetemp))!=EOF){
+		while((ret=fscanf(fp,"%d,	%lf",&notemp,&valuetemp))!=EOF){
 				no[i]=notemp;
 				value[i]=valuetemp;
 				i++;
@@ -19,11 +23,11 @@ int	main(void){
     //データを何列読み込んだかカウント
 
 		fclose(fp);
+		
     //データを書きこむ
     fp=fopen("6-2.csv","w");
   for(j=0;j<i;j++){
-      fprintf(fp,"%d,
-%f\n",no[j],value[j]);
+      fprintf(fp,"%d,%f\n",no[j],value[j]);
   }
   fclose(fp);
   return	0;
